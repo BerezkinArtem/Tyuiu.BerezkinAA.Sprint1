0702060227516f77c6fc51bd558a29a649dc974c
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-10-02 18:01:28 +0500

    Task1

--- Tyuiu.BerezkinAA.Sprint1.Task1.V18/Program.cs
+++ Tyuiu.BerezkinAA.Sprint1.Task1.V18/Program.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.BerezkinAA.Sprint1.Task1.V18
             Console.Title = "Спринт #1 | Выполнил: Березкин А. А. | ИСПб-24-1";
             Console.WriteLine("***********************************************************************************");
             Console.WriteLine("* Спринт #1                                                                       *");
-            Console.WriteLine("* Тема: Базовые навыки работы в C#                                                *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                         *");
             Console.WriteLine("* Задание #1                                                                      *");
             Console.WriteLine("* Вариант #18                                                                     *");
             Console.WriteLine("* Выполнил: Березкин Артем Андреевич | ИСПб-24-1                                  *");
