98d02e705cb15952eceac01521b50258ac27549f
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-10-02 18:04:24 +0500

    Task5

--- Tyuiu.BerezkinAA.Sprint1.Task5.V7/Program.cs
+++ Tyuiu.BerezkinAA.Sprint1.Task5.V7/Program.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.BerezkinAA.Sprint1.Task5.V7
             Console.Title = "Спринт #1 | Выполнил: Березкин А. А. | ИСПб-24-1";
             Console.WriteLine("*************************************************************************************");
             Console.WriteLine("* Спринт #1                                                                         *");
-            Console.WriteLine("* Тема: Базовые навыки работы в C#                                                  *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                                        *");
             Console.WriteLine("* Задание #5                                                                        *");
             Console.WriteLine("* Вариант #7                                                                        *");
             Console.WriteLine("* Выполнил: Березкин Артем Андреевич | ИСПб-24-1                                    *");
